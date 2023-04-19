9310c87ca68acdf68dbcdf865bec0ffd4d42f8f7
Natanaelvich <taelima1997@gmail.com>
2023-04-18 23:20:10 -0300

    add login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,54 @@
-import { Link } from "expo-router";
-import {  View } from "react-native";
+import { Link, useNavigation, useRouter, useSegments } from "expo-router";
+import { useState } from "react";
+import { Alert, Button, Pressable, Text, TextInput, View } from "react-native";
 
-export default function Home() {
-    return (
-        <View style={{alignItems : 'center', justifyContent : 'center', flex : 1}}>
-          <Link href="/settings">Seetings</Link>
-        </View>
-      );
+export default function Login() {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    router.push("/home");
+  };
+
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-900">
+      <View className="w-3/4">
+        <Text className="text-white text-3xl font-bold mb-8 text-center">
+          Expo router example
+        </Text>
+
+        <TextInput
+          className="border border-gray-400 p-2 mb-4 rounded bg-gray-800 text-white placeholder-gray-500 focus:border-blue-500 focus:outline-none"
+          placeholder="Username"
+          placeholderTextColor="#718096"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          className="border border-gray-400 p-2 mb-4 rounded bg-gray-800 text-white placeholder-gray-500 focus:border-blue-500 focus:outline-none"
+          placeholder="Password"
+          placeholderTextColor="#718096"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "#4299e1" : "#3182ce",
+              borderRadius: 4,
+              padding: 12,
+              alignItems: "center",
+            },
+            pressed ? { opacity: 0.6 } : null,
+          ]}
+          onPress={handleLogin}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Login</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
