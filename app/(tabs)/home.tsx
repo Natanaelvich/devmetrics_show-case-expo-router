80be8983e1d944cf6445a03cc2e37a282a2f2b2a
Natanaelvich <taelima1997@gmail.com>
2023-04-20 22:58:53 -0300

    update home to get user github

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,32 +1,44 @@
-import { Link } from "expo-router";
+import { useSearchParams } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Image, Pressable, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { ActivityIndicator, Image, Text, View } from "react-native";
 
 export default function Home() {
+  const { githubUsername } = useSearchParams();
+
+  const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (githubUsername) {
+      fetch(`https://api.github.com/users/${githubUsername}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setUserData(data);
+          setIsLoading(false);
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [githubUsername]);
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
       <StatusBar style="light" />
       <Text className="text-white text-3xl font-bold mb-8">
         Bem-vindo ao Meu App!
       </Text>
-      <Image
-        source={{ uri: "https://source.unsplash.com/200x200/?avatar" }}
-        className="w-32 h-32 rounded-full mb-8"
-      />
-      <Text className="text-lg text-center text-gray-300 px-4">
-        Nosso aplicativo é uma ferramenta incrível que ajuda você a gerenciar
-        sua vida de uma forma mais eficiente. Com Meu App, você pode facilmente
-        organizar suas tarefas, criar listas de compras e muito mais!
-      </Text>
 
-      <Pressable>
-        <Text className="text-white text-center mt-4">
-          Go to{" "}
-          <Link href="dashboard" className="text-blue-500">
-            Dashboard
-          </Link>
-        </Text>
-      </Pressable>
+      {isLoading && <ActivityIndicator size="large" color="#fff" />}
+      {userData && (
+        <>
+          <Image
+            source={{ uri: userData.avatar_url }}
+            className="w-32 h-32 rounded-full mb-8"
+          />
+          <Text className="text-white text-2xl mb-4">{userData.name}</Text>
+          <Text className="text-white text-lg mb-8">{userData.bio}</Text>
+        </>
+      )}
     </View>
   );
 }
