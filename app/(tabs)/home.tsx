25fd4720fbbcbeea16bbb72186ae748dff612fea
Natanaelvich <taelima1997@gmail.com>
2023-04-21 10:19:52 -0300

    update home with card buttons

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,13 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
-import { ActivityIndicator, Image, Text, View } from "react-native";
+import { ActivityIndicator, Image, Pressable, Text, View } from "react-native";
 import { useGithubStore } from "../../store/github-store";
+import { useRouter } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function Home() {
   const { username } = useGithubStore();
+  const router = useRouter();
 
   const [userData, setUserData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+
+  const handleRepositoriesPress = () => {
+    router.push("repositories");
+  };
+
+  const handleAccountDetailsPress = () => {
+    router.push("accountDetails");
+  };
+
+  const handleMetricsPress = () => {
+    router.push("metrics");
+  };
 
   useEffect(() => {
     if (username) {
@@ -37,6 +52,30 @@ export default function Home() {
           />
           <Text className="text-white text-2xl mb-4">{userData.name}</Text>
           <Text className="text-white text-lg mb-8">{userData.bio}</Text>
+
+          <View className="flex flex-row justify-around w-full mt-8 px-6">
+            <Pressable
+              className="bg-gray-800 rounded-md p-4 flex items-center justify-center w-1/3 mr-4"
+              onPress={handleRepositoriesPress}
+            >
+              <Feather name="github" size={32} color="#fff" />
+              <Text className="text-white font-bold ml-2">Repositories</Text>
+            </Pressable>
+            <Pressable
+              className="bg-gray-800 rounded-md p-4 flex items-center justify-center w-1/3 mx-2"
+              onPress={handleAccountDetailsPress}
+            >
+              <Feather name="user" size={32} color="#fff" />
+              <Text className="text-white font-bold ml-2">Account Details</Text>
+            </Pressable>
+            <Pressable
+              className="bg-gray-800 rounded-md p-4 flex items-center justify-center w-1/3 ml-4"
+              onPress={handleMetricsPress}
+            >
+              <Feather name="bar-chart" size={32} color="#fff" />
+              <Text className="text-white font-bold ml-2">Metrics</Text>
+            </Pressable>
+          </View>
         </>
       )}
     </View>
