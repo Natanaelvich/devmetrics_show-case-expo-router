9256aa580b2817f2a457a974fc643b6fae092d38
Natanael <taelima1997@gmail.com>
2023-06-12 23:18:27 -0300

    add button close

---
+++ src/components/ButtonClose.tsx
@@ -0,0 +1,20 @@
+import { useNavigation } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+const ButtonClose = () => {
+    const navigation  = useNavigation()
+
+    return (
+        <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={() => {
+            navigation.goBack();
+          }}
+      >
+        <MaterialIcons name="close" size={24} color="#CBD5E0" />
+      </TouchableOpacity>
+    );
+};
+
+export default ButtonClose;
