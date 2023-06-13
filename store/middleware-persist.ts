38397a8d163acf0d4984c4712feed3d075776d78
Natanael <taelima1997@gmail.com>
2023-06-12 23:36:06 -0300

    add initial zustando store

---
+++ store/middleware-persist.ts
@@ -0,0 +1,18 @@
+import { StateStorage } from 'zustand/middleware'
+
+
+const zustandStorage: StateStorage = {
+  setItem: (name, value) => {
+    return console.log(`Persisting ${name} to ${value}`)
+  },
+  getItem: (name) => {
+    const value = 'test'
+    console.log(`Restoring ${name} to ${value}`)
+    return value
+  },
+  removeItem: (name) => {
+    return console.log(`Removing ${name}`)
+  },
+}
+
+export default zustandStorage
