3277cc41bca84788d353cba510732bb2cff3a398
Natanael <taelima1997@gmail.com>
2023-06-12 23:36:29 -0300

    update with persist with zustand store middleware

--- store/github-store.ts
+++ store/github-store.ts
@@ -1,11 +1,21 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import zustandStorage from "./middleware-persist";
 
 type GithubStore = {
   username: string;
   setUsername: (username: string) => void;
 };
 
-export const useGithubStore = create<GithubStore>((set) => ({
-  username: "",
-  setUsername: (username: string) => set({ username }),
-}));
+export const useGithubStore = create(
+  persist<GithubStore>(
+    (set) => ({
+      username: "",
+      setUsername: (username: string) => set({ username }),
+    }),
+    {
+      name: "github-storage",
+      storage: createJSONStorage(() => zustandStorage),
+    }
+  )
+);
